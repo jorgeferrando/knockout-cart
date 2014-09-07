1656d06c35089dd6bf513c558e617a3bbdceccc5
jorgeferrando <jorgeparent@gmail.com>
2014-09-07 17:13:09 +0200

    product obsevable and table

--- js/models/product.js
+++ js/models/product.js
@@ -1,9 +1,17 @@
 function Product(id,name,price,stock) {
     "use strict";
+
+    var
+        _id = ko.observable(id),
+        _name = ko.observable(name),
+        _price = ko.observable(price),
+        _stock = ko.observable(stock)
+    ;
+
     return {
-        id:id,
-        name:name,
-        price:price,
-        stock:stock
+        id:_id,
+        name:_name,
+        price:_price,
+        stock:_stock
     };
 }
