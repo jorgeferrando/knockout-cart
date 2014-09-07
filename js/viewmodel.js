7b0eba16a4887ba109bb11bc304f3cc5adf62ce2
jorgeferrando <jorgeparent@gmail.com>
2014-09-07 19:00:52 +0200

    observableArray

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -1,11 +1,40 @@
-
 var vm = (function () {
     "use strict";
 
-    var product = new Product(1,"T-Shirt", 10.00, 20);
+    var catalog = ko.observableArray([
+        new Product(1, "T-Shirt", 10.00, 20),
+        new Product(1, "Trousers", 20.00, 10),
+        new Product(1, "Shirt", 15.00, 20),
+        new Product(1, "Shorts", 5.00, 10)
+    ]);
+
+    var newProduct = {
+        name:ko.observable(),
+        price:ko.observable(),
+        stock:ko.observable(),
+        clear: function() {
+            this.name("");
+            this.price("");
+            this.stock("");
+        }
+    };
+
+    var addProduct = function (data) {
+        var id = new Date().valueOf();
+        var product = new Product(
+            id,
+            data.name(),
+            data.price(),
+            data.stock()
+        );
+        vm.catalog.push(product);
+        newProduct.clear();
+    };
 
     return {
-        product: product
+        catalog: catalog,
+        newProduct: newProduct,
+        addProduct: addProduct
     };
 })();
 
