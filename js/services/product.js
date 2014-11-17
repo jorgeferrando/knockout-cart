59e50fc221edb14ef4baf2e043d18610cc966b6d
jorgeferrando <jorgeparent@gmail.com>
2014-11-17 20:59:59 +0100

    ajax, mock and promise libraries added

---
+++ js/services/product.js
@@ -0,0 +1,19 @@
+$(function (ko){
+    "use strict";
+    function ProductService(options) {
+        function all() {
+
+        }
+        function get(id) {
+
+        }
+        function save() {
+
+        }
+        return {
+            all:all,
+            get:get,
+            save: save
+        };
+    }
+})(ko);
